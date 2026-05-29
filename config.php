775d73da7c45052b7cefd6d6824d3d384072c569
china2dz <inouinou346@gmail.com>
2026-05-29 21:04:50 +0100

    fix db config

--- config.php
+++ config.php
@@ -1,15 +1,16 @@
 <?php
-$host     = 'localhost';
-$db       = 'china2dz';
-$user     = 'root';
-$pass     = '';
-$charset  = 'utf8mb4';
+$host    = getenv('MYSQLHOST');
+$db      = getenv('MYSQLDATABASE');
+$user    = getenv('MYSQLUSER');
+$pass    = getenv('MYSQLPASSWORD');
+$port    = getenv('MYSQLPORT') ?: '3306';
+$charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 
 try {
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die(json_encode(['error' => 'Connection failed: '. $e->getMessage()]));
+    die(json_encode(['error' => 'Connection failed: ' . $e->getMessage()]));
 }
